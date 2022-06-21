1bd164f8d7fbe29550e3fd2002ed3073bc715150
Aelida <lidasouza89@gmail.com>
2022-06-21 19:18:27 -0300

    Inclusão Requisito 4

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    return (
+      <div data-testid="planet-card" />
+
+    );
+  }
+}
+
+export default PlanetCard;
