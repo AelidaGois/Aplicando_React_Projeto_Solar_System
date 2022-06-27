dfd63545fd35533d61c587bbfa1b9df2ad8bac35
Aelida <lidasouza89@gmail.com>
2022-06-27 20:21:21 -0300

    Estilização css

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -6,8 +6,10 @@ import missions from '../data/missions';
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
-        <Title headline="Missões" />
+      <div data-testid="missions" className="missions">
+        <div className="mission-title">
+          <Title headline="Missões" />
+        </div>
         {missions.map((mission) => (
           <MissionCard
             key={ mission.name }
